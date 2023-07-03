c00b660db5e9f7676e6ed04ccc71c5d61e421284
shinyay <shinya.com@gmail.com>
2023-07-03 15:28:15 +0900

    Add a logic on Simple Logger

--- Day033/my-logging-app/src/lib.rs
+++ Day033/my-logging-app/src/lib.rs
@@ -1,15 +1,35 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use log::{LevelFilter, trace, info};
+use simple_logger::SimpleLogger;
 use spin_sdk::{
     http::{Request, Response},
-    http_component,
+    http_component, config,
 };
 
 /// A simple Spin HTTP component.
 #[http_component]
 fn handle_my_logging_app(req: Request) -> Result<Response> {
+    init_logger()?;
+    trace!("method={}, uri={}", req.method(), req.uri());
+    info!("returning 200");
+
     println!("{:?}", req.headers());
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
         .body(Some("Hello, Fermyon".into()))?)
+}
+
+fn init_logger() -> Result<()> {
+    const LOG_LEVEL_CONFIG_VARIABLE: &str = "loglevel";
+
+    let level: LevelFilter = config::get(LOG_LEVEL_CONFIG_VARIABLE)?
+        .parse()
+        .map_err(|e| anyhow!("parsing log level: {e}"))?;
+
+    SimpleLogger::new()
+        .with_level(level)
+        .init()?;
+
+    Ok(())
 }
