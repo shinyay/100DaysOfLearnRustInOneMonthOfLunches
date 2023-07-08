0423aaeb216af85778cb8e1a29c9ee532297aa41
shinyay <shinya.com@gmail.com>
2023-07-08 16:26:11 +0900

    Add Httpbin access sample

--- Day037/http-call-sample/src/main.rs
+++ Day037/http-call-sample/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    match get("http://httpbin.org/get").await {
+        Ok(response) => {
+            let result = response.json::<GetBody>().await;
+            match result {
+                Ok(json) => {
+                    println!("{:#?}", json);
+                }
+                Err(err) => {
+                    println!("{:#?}", err)
+                }
+            }
+        }
+        Err (err) => {
+            println!("{:#?}", err)
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct GetBody {
+    args: HashMap<String, String>,
+    headers: HashMap<String, String>,
+    origin: String,
+    url: String,
 }
