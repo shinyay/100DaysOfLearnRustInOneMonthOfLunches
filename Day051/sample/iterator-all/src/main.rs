abe811331d6005f1393879dc607fcbe5091a4de7
shinyay <shinya.com@gmail.com>
2023-07-22 12:37:39 +0900

    Add sample code for all method with iterator

--- Day051/sample/iterator-all/src/main.rs
+++ Day051/sample/iterator-all/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let my_vec: Vec<i32> = vec![1, 2, 3];
+    println!("Negative number is included?:  {}", my_vec.iter().all(|&item| item.is_negative()));
+
+    let my_vec: Vec<i32> = vec![-1, 0, 1, 2, 3];
+    println!("Negative number is included?:  {}", my_vec.iter().all(|&item| item.is_negative()));
+
+    let my_vec: Vec<i32> = vec![-1, -2, -3];
+    println!("Negative number is included?:  {}", my_vec.iter().all(|&item| item.is_negative()));
 }
