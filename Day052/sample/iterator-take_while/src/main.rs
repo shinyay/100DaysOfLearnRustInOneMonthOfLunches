b55c240b19b7ac82fe7233a899e33b68361b753c
shinyay <shinya.com@gmail.com>
2023-07-23 22:59:42 +0900

    Add sample for take_while

--- Day052/sample/iterator-take_while/src/main.rs
+++ Day052/sample/iterator-take_while/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let a = [-1, 0, 1];
+    let mut result = a.iter().take_while(|x| **x < 0);
+    println!("{:?}", result.next());
+    println!("{:?}", result.next());
 }
