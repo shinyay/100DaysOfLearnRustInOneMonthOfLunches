50548870954c8f705ebb9dd216fd626052d7fe62
shinyay <shinya.com@gmail.com>
2023-07-23 22:11:44 +0900

    Add zip and cycle sanoke

--- Day052/sample/iterator-zip/src/main.rs
+++ Day052/sample/iterator-zip/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let even_odd_vec = vec!["even", "odd"];
+
+    let my_vec = (0..10)
+        .zip(even_odd_vec.into_iter().cycle())
+        .collect::<Vec<(i32, &str)>>();
+
+    println!("{:?}", my_vec);
 }
