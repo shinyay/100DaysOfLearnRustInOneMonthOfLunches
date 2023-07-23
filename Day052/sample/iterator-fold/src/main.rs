a1efc4322ef1867ec5a59eda3d9444beaacd3dab
shinyay <shinya.com@gmail.com>
2023-07-23 22:21:54 +0900

    Add fold sample

--- Day052/sample/iterator-fold/src/main.rs
+++ Day052/sample/iterator-fold/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let some_numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ 
+    println!("{}", some_numbers
+        .iter()
+        .fold(0, |total_so_far, next_number| total_so_far + next_number)
+    );
 }
