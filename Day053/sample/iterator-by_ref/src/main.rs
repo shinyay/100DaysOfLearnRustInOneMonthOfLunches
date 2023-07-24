24368da8dc1a32f9b217f3f4f54706aa2ba52227
shinyay <shinya.com@gmail.com>
2023-07-24 19:17:48 +0900

    Add by_ref method sample

--- Day053/sample/iterator-by_ref/src/main.rs
+++ Day053/sample/iterator-by_ref/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut my_vec = vec!["Hello", "Rust", "on", "Jul 23"].into_iter();
+    let hello: Vec<_> = my_vec.by_ref().take(2).collect();
+
+    println!("MSG: {:?}", hello);
+
+    let date: Vec<_> = my_vec.collect();
+
+    println!("DATE: {:?}", date);
 }
