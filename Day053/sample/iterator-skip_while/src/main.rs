1b2c2c6c6ca1aecb98c6e2803ece8dfd88341e2f
shinyay <shinya.com@gmail.com>
2023-07-24 18:11:49 +0900

    Add skip_while sample

--- Day053/sample/iterator-skip_while/src/main.rs
+++ Day053/sample/iterator-skip_while/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let my_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let result = my_vec.iter().skip_while(|x| **x < 5).collect::<Vec<_>>();
+
+    println!("{:?}", result);
 }
