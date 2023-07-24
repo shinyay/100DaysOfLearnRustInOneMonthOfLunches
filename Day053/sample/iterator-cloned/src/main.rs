aa37086be7e7e2ccc03ade180d0e3c7f902eaf6f
shinyay <shinya.com@gmail.com>
2023-07-24 17:41:12 +0900

    Add sample for cloned

--- Day053/sample/iterator-cloned/src/main.rs
+++ Day053/sample/iterator-cloned/src/main.rs
@@ -2,11 +2,13 @@ fn main() {
     let my_vec = vec![1, 2, 3, 4, 5];
 
     let cloned_vec = my_vec.iter().cloned().collect::<Vec<_>>();
-    let mapped_vec = my_vec.iter().map(|x| x).collect::<Vec<_>>();
+    let mapped_vec = my_vec.iter().map(|x| *x).collect::<Vec<_>>();
     let clone_vec = my_vec.iter().map(|x| x.clone()).collect::<Vec<_>>();
 
     println!("{:?}", cloned_vec);
     println!("{:?}", mapped_vec);
     println!("{:?}", clone_vec);
     assert_eq!(cloned_vec, my_vec);
+    assert_eq!(cloned_vec, mapped_vec);
+    assert_eq!(cloned_vec, clone_vec);
 }
