34bb7ef71df459bcc26bd9bcf3412e864ea00eab
shinyay <shinya.com@gmail.com>
2023-07-24 17:33:35 +0900

    Add sample for cloned

--- Day053/sample/iterator-cloned/src/main.rs
+++ Day053/sample/iterator-cloned/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let my_vec = vec![1, 2, 3, 4, 5];
+
+    let cloned_vec = my_vec.iter().cloned().collect::<Vec<_>>();
+    let mapped_vec = my_vec.iter().map(|x| x).collect::<Vec<_>>();
+
+    println!("{:?}", cloned_vec);
+
 }
