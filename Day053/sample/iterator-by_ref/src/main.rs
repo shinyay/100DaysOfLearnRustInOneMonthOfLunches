1085fe6e7fd53bd6ae91685a406b5d12b17f02e2
shinyay <shinya.com@gmail.com>
2023-07-24 19:20:05 +0900

    Add by_ref method sample

--- Day053/sample/iterator-by_ref/src/main.rs
+++ Day053/sample/iterator-by_ref/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let mut my_vec = vec!["Hello", "Rust", "on", "Jul 23"].into_iter();
     let hello: Vec<_> = my_vec.by_ref().take(2).collect();
+    // let hello: Vec<_> = my_vec.take(2).collect(); // error[E0382]: use of moved value: `my_vec`
 
     println!("MSG: {:?}", hello);
 
