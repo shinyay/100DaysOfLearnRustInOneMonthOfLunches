f0c2029b4bd86baaed0ce5f7b067a211858b4aad
shinyay <shinya.com@gmail.com>
2023-07-25 17:43:33 +0900

    Add a sample for chunks

--- Day054/sample/iterator-chunks/src/main.rs
+++ Day054/sample/iterator-chunks/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let my_slice = ['H', 'e', 'l', 'l', 'o', 'R', 'u', 's', 't', '!'];
+    println!("Chunk size: 3");
+    for chunk in my_slice.chunks(3) {
+        println!("{:?}", chunk);
+    }
+    println!("Chunk size: 5");
+    for chunk in my_slice.chunks(5) {
+        println!("{:?}", chunk);
+    }
 }
