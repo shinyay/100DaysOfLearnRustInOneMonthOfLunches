322e86d078cb418f76229db7ec26831abb344dce
shinyay <shinya.com@gmail.com>
2023-07-25 19:44:50 +0900

    Add peekable sample

--- Day054/sample/iterator-peekable/src/main.rs
+++ Day054/sample/iterator-peekable/src/main.rs
@@ -2,8 +2,8 @@ fn main() {
     let mut my_iter = [1, 2, 3, 4, 5].iter().peekable();
 
     for _ in 0..6 {
-        println!("{:?}", my_iter.peek());
-        println!("{}", my_iter.peek().unwrap());
+        println!("Option: {:?}", my_iter.peek());
+        println!("Value: {}", my_iter.peek().unwrap());
         my_iter.next();
     }
 }
