3fdc40ee60859ad6a8137a4383e6c5f9a9964f8f
shinyay <shinya.com@gmail.com>
2023-07-26 16:08:05 +0900

    Add inspect method sample, which outputs debug log

--- Day055/sample/iterator-inspect/src/main.rs
+++ Day055/sample/iterator-inspect/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let my_vec = [1, 2, 3, 4, 5];
+
+    let sum = my_vec.iter()
+        .cloned()
+        .filter(|x| x % 2 == 0)
+        .fold(0, |sum, i| sum + i);
+    
+    println!("{sum}");
+    
+    let sum = my_vec.iter()
+        .cloned()
+        .inspect(|x| println!("Current item: {x}"))
+        .filter(|x| x % 2 == 0)
+        .inspect(|x| println!("Current item through Filter: {x}"))
+        .fold(0, |sum, i| sum + i);
+    
+    println!("{sum}");
 }
