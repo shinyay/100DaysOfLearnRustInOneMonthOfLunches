7401cf80779caf73c5cdf62beafeda6aed2d0e7d
shinyay <shinya.com@gmail.com>
2023-07-26 15:19:31 +0900

    Add dgb macro sample

--- Day055/sample/dbg-macro/src/main.rs
+++ Day055/sample/dbg-macro/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
     let my_number = 1;
     dbg!(my_number);
+    println!("println!: {my_number}");
 
     let my_string = "Hello Rust";
     dbg!(my_string);
+    println!("println!: {my_string}");
 
     let my_vec = vec!['a', 'b', 'c'];
     dbg!(my_vec);
