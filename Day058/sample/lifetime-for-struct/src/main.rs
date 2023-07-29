c7658537a32f145570273bdd252a333b14fa931b
shinyay <shinya.com@gmail.com>
2023-07-29 15:37:24 +0900

    Add Display trait

--- Day058/sample/lifetime-for-struct/src/main.rs
+++ Day058/sample/lifetime-for-struct/src/main.rs
@@ -1,3 +1,29 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8,
+}
+
+impl Person<'_> {
+    fn say_hello(&self) {
+        println!("{} says Hello", self.name);
+    }
+}
+
+impl Display for Person<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}({}) says Hello", self.name, self.age)
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let me = Person {
+        name: "Shinya",
+        age: 40,
+    };
+
+    println!("{:?}", me);
+    println!("{}", me);
+    me.say_hello();
 }
