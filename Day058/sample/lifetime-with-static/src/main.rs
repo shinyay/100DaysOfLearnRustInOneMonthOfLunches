7d2a174052b39ca2a891e822e6d9922b6a4d4989
shinyay <shinya.com@gmail.com>
2023-07-29 14:42:04 +0900

    Add lifetime sample

--- Day058/sample/lifetime-with-static/src/main.rs
+++ Day058/sample/lifetime-with-static/src/main.rs
@@ -1,6 +1,23 @@
+// #[derive(Debug)]
+// struct Person {
+//     name: & 'static str,
+//     age: u32,
+// }
+ 
+// fn main() {
+//     let people_vec = vec!["Yanagihara".to_string(), "John".to_string()];
+
+//     let me = Person {
+//         name: &people_vec[0],
+//         age: 30,
+//     };
+
+//     println!("{:?}", me);
+// }
+
 #[derive(Debug)]
-struct Person {
-    name: & 'static str,
+struct Person<'a> {
+    name: & 'a str,
     age: u32,
 }
  
