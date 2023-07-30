2fa9a8bbb92188679d2248d9d6fff0e3147627db
shinyay <shinya.com@gmail.com>
2023-07-30 15:16:24 +0900

    Add sample for Cell, iterior mutability

--- Day060/sample/interior-mutability-cell/src/main.rs
+++ Day060/sample/interior-mutability-cell/src/main.rs
@@ -1,3 +1,24 @@
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug)]
+struct Book {
+    author: String,
+    title: String,
+    category: String,
+    price: u32,
+    sale: bool,
+}
+
+impl Book {
+    
+}
+
+impl Display for Book {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "")
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
