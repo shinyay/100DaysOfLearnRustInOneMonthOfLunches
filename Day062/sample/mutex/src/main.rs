c28aa08c674d24208bb9ec5d6602c2a4997a27b1
shinyay <shinya.com@gmail.com>
2023-08-01 19:10:23 +0900

    Add a sample for Mutex

--- Day062/sample/mutex/src/main.rs
+++ Day062/sample/mutex/src/main.rs
@@ -1,3 +1,8 @@
+use std::sync::Mutex;
+
 fn main() {
-    println!("Hello, world!");
+    let my_mutex = Mutex::new(10);
+    let mut mutex_changer = my_mutex.lock().unwrap();
+
+    println!("{:?}", mutex_changer);
 }
