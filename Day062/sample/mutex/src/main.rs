60535b710fdeea70265057ba8ca700ed4af8d262
shinyay <shinya.com@gmail.com>
2023-08-01 19:22:43 +0900

    Add a sample for Mutex

--- Day062/sample/mutex/src/main.rs
+++ Day062/sample/mutex/src/main.rs
@@ -4,5 +4,23 @@ fn main() {
     let my_mutex = Mutex::new(10);
     let mut mutex_changer = my_mutex.lock().unwrap();
 
+    println!("{:?}", my_mutex);
     println!("{:?}", mutex_changer);
+
+    *mutex_changer *= 10;
+
+
+    println!("my_mutex: {:?}", my_mutex);
+    println!("{:?}", mutex_changer);
+
+    let your_mutex = Mutex::new(10);
+    {
+        let mut mutex_changer = your_mutex.lock().unwrap();
+        *mutex_changer += 1;
+
+        println!("your_mutex: {:?}", your_mutex);
+        println!("{:?}", mutex_changer);
+    }
+
+    println!("your_mutex: {:?}", your_mutex);
 }
