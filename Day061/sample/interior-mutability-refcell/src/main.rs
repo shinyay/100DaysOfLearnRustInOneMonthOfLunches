4cc894b9c524943fc0c40c9299f6fc9ca94f7f2f
shinyay <shinya.com@gmail.com>
2023-08-01 10:33:15 +0900

    Add debug output with Formatter

--- Day061/sample/interior-mutability-refcell/src/main.rs
+++ Day061/sample/interior-mutability-refcell/src/main.rs
@@ -1,3 +1,33 @@
+use std::{cell::RefCell, fmt::{Display, Formatter, Result}};
+
+// #[derive(Debug)]
+// struct Book {
+//     author: String,
+//     title: String,
+//     category: String,
+//     price: u32,
+//     sale: bool,
+// }
+
+#[derive(Debug)]
+struct Book {
+    author: String,
+    title: String,
+    category: String,
+    price: u32,
+    sale: RefCell<bool>,
+}
+
+impl Book {
+    
+}
+
+impl Display for Book {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        todo!()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
