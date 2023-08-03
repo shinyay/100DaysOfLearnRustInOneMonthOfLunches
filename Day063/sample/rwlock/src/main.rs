2451fcb5cb05ba76affb8f024cc309095aafbf4b
shinyay <shinya.com@gmail.com>
2023-08-03 12:06:24 +0900

    Add drop

--- Day063/sample/rwlock/src/main.rs
+++ Day063/sample/rwlock/src/main.rs
@@ -1,3 +1,16 @@
+use std::sync::RwLock;
+ 
 fn main() {
-    println!("Hello, world!");
+    let my_rwlock = RwLock::new(1);
+ 
+    let my_read = my_rwlock.read().unwrap();
+    println!("{:?}", my_read);
+ 
+    // This program runs forever withoud `drop()`
+    drop(my_read);
+
+    let mut my_write = my_rwlock.write().unwrap();
+    *my_write += 1;
+
+    println!("{:?}", my_rwlock);
 }
