56df4a2d8f273fb1c9c64fc2a55d43a8319e6d31
shinyay <shinya.com@gmail.com>
2023-08-03 13:17:55 +0900

    Add a project template for RwLock with try_read and try_write

--- Day063/sample/rwlock-try-read/src/main.rs
+++ Day063/sample/rwlock-try-read/src/main.rs
@@ -1,3 +1,17 @@
+use std::sync::RwLock;
+ 
 fn main() {
-    println!("Hello, world!");
+    let my_rwlock = RwLock::new(1);
+
+    let my_read = my_rwlock.read().unwrap();
+    println!("The value from RwLock: {}", my_read);
+
+    drop(my_read);
+
+    if let Ok(mut value) = my_rwlock.try_write() {
+        *value += 10;
+        println!("The value from RwLock: {}", value);
+    } else {
+        println!("Can't Get Write Access")
+    };
 }
