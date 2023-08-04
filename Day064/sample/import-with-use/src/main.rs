35e6df3a628fb42d3d90ce7aab8620e5e6534b0a
shinyay <shinya.com@gmail.com>
2023-08-04 15:09:04 +0900

    Add sample for use as import

--- Day064/sample/import-with-use/src/main.rs
+++ Day064/sample/import-with-use/src/main.rs
@@ -1,3 +1,17 @@
+enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+}
 fn main() {
     println!("Hello, world!");
 }
