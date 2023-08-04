8094ad5d8833f2f6d6335d02c9a7545d201a45f8
shinyay <shinya.com@gmail.com>
2023-08-04 15:09:17 +0900

    Add enum sample

--- Day064/sample/import-with-use/src/main.rs
+++ Day064/sample/import-with-use/src/main.rs
@@ -12,6 +12,26 @@ enum Month {
     November,
     December,
 }
+
+fn select_month(month: &Month) {
+    match month {
+        Month::January => println!("It's Winter."),
+        Month::February => println!("It's Winter."),
+        Month::March => println!("It's Spring."),
+        Month::April => println!("It's Spring."),
+        Month::May => println!("It's Spring."),
+        Month::June => println!("It's Summer."),
+        Month::July => println!("It's Summer."),
+        Month::August => println!("It's Summer."),
+        Month::September => println!("It's Fall."),
+        Month::October => println!("It's Fall."),
+        Month::November => println!("It's Fall."),
+        Month::December => println!("It's Winter."),
+        _ => println!("Etc."),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let month = Month::January;
+    select_month(&month);
 }
