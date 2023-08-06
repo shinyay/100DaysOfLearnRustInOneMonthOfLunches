107cb4970b91947152cad13af3b48a4e15574ab4
shinyay <shinya.com@gmail.com>
2023-08-06 20:14:03 +0900

    Add mismatched type error sample

--- Day066/sample/type-aliase/src/main.rs
+++ Day066/sample/type-aliase/src/main.rs
@@ -1,1 +1,9 @@
 type MyCharVec = Vec<char>;
+
+fn main() {
+    let mut my_chars: MyCharVec = Vec::new();
+    
+    my_chars.push(1); // error[E0308]: mismatched types - expected `char`, found `u8`
+
+    println!("MyCharVec: {:?}", my_chars);
+}
