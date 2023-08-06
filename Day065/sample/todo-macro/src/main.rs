d0a19851924bd742e434b51978a33a40b3f62f21
shinyay <shinya.com@gmail.com>
2023-08-06 13:01:56 +0900

    Add a sample code as imcompleted

--- Day065/sample/todo-macro/src/main.rs
+++ Day065/sample/todo-macro/src/main.rs
@@ -1,3 +1,26 @@
+struct Book {}
+
+enum BookCategory {
+    Mistery,
+    Fantasy,
+    Action,
+    Technology,
+}
+ 
+fn get_book(book: &Book) -> Option<String> {} 
+
+fn delete_book(book: &Book) -> Result<(), String> {} 
+
+fn check_book_category(book_category: &BookCategory) {
+    match book_category {
+        BookCategory::Action => println!("The Category is Action"),
+        BookCategory::Fantasy => println!("The Category is Fantasy"),
+        BookCategory::Mistery => println!("The Category is Mistery"),
+        BookCategory::Technology => println!("The Category is Technology"),
+    }
+}
+ 
 fn main() {
-    println!("Hello, world!");
+    let book_type = BookType::HardCover;
+    check_book_type(&book_type);
 }
