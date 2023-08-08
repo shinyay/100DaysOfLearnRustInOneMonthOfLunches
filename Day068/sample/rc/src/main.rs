e79317c84b37bd81913a0866f20b4f5f230bdffe
shinyay <shinya.com@gmail.com>
2023-08-08 14:57:12 +0900

    Add Rc sample

--- Day068/sample/rc/src/main.rs
+++ Day068/sample/rc/src/main.rs
@@ -1,3 +1,11 @@
+use std::rc::Rc;
+
 fn main() {
-    println!("Hello, world!");
+    let my_string: Rc<String> = Rc::new("Hello Rc in the Rust".to_string());
+    let cloned_string1 = my_string.clone();
+    let cloned_string2 = my_string.clone();
+
+    println!("my_string: {:p}", &*my_string);
+    println!("cloned_string1: {:p}", &*cloned_string1);
+    println!("cloned_string2: {:p}", &*cloned_string2);
 }
