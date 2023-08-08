498816e46528931746932667516a5482696d8275
shinyay <shinya.com@gmail.com>
2023-08-08 16:03:02 +0900

    Modify sample code

--- Day068/sample/rc/src/main.rs
+++ Day068/sample/rc/src/main.rs
@@ -1,11 +1,21 @@
 use std::rc::Rc;
 
 fn main() {
-    let my_string: Rc<String> = Rc::new("Hello Rc in the Rust".to_string());
+    let my_string = "Hello Rc".to_string();
     let cloned_string1 = my_string.clone();
     let cloned_string2 = my_string.clone();
 
-    println!("my_string: {:p}", &*my_string);
+    println!("     my_string: {:p}", &*my_string);
+    println!("cloned_string1: {:p}", &*cloned_string1);
+    println!("cloned_string2: {:p}", &*cloned_string2);
+
+    println!("------------------------------");
+
+    let my_string: Rc<String> = Rc::new("Hello Rc".to_string());
+    let cloned_string1 = my_string.clone();
+    let cloned_string2 = my_string.clone();
+
+    println!("     my_string: {:p}", &*my_string);
     println!("cloned_string1: {:p}", &*cloned_string1);
     println!("cloned_string2: {:p}", &*cloned_string2);
 }
