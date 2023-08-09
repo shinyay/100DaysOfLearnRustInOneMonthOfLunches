66a86c9acd006e87ac0666c244e274cf85033a41
shinyay <shinya.com@gmail.com>
2023-08-09 18:45:55 +0900

    Add a sample for thread with JoinHandle

--- Day069/sample/spawn-handle/src/main.rs
+++ Day069/sample/spawn-handle/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    for count in 1..10 {
+        let my_hundle = std::thread::spawn(move || {
+            println!("[{}] Hello, thread!", count)
+        });
+
+        my_hundle.join();
+    }
 }
