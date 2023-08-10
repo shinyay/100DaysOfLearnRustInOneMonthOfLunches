2352c3f2e177a6b7e08d5a8d4885b8ed08485173
shinyay <shinya.com@gmail.com>
2023-08-10 11:59:46 +0900

    Add a project template for Thread

--- Day069/sample/spawn-handle-vector/src/main.rs
+++ Day069/sample/spawn-handle-vector/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut my_vec = vec![];
+    for count in 0..10 {
+        my_vec.push(
+          std::thread::spawn(move || {
+            println!("[{}] Hello, thread!", count);
+          })  
+        );
+    }
+
+    for handle in my_vec {
+        handle.join().unwrap();
+    }
 }
