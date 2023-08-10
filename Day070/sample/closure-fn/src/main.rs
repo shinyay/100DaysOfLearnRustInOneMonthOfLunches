029e4cd546a63144a4ed3d5ffbf9780ec1f048cd
shinyay <shinya.com@gmail.com>
2023-08-10 17:48:01 +0900

    Add a sample for closure with Fn trait

--- Day070/sample/closure-fn/src/main.rs
+++ Day070/sample/closure-fn/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let default_value = 10;
+    let my_closure = |x: u32| {
+        println!("Variable: {}", x);
+        default_value * x
+    };
+
+    let result = my_closure(1);
+    assert_eq!(10, result);
+    println!("Result: {:?}", result);
+
+    let result = my_closure(5);
+    assert_eq!(50, result);
+    println!("Result: {:?}", result);
+
+    let result = my_closure(10);
+    assert_eq!(100, result);
+    println!("Result: {:?}", result);
 }
