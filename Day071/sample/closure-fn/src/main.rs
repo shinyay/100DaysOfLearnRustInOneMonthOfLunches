79aaa1961d60964735a7418822ca2d110ba70229
shinyay <shinya.com@gmail.com>
2023-08-11 17:51:29 +0900

    Add a sample code of the closure with Fn

--- Day071/sample/closure-fn/src/main.rs
+++ Day071/sample/closure-fn/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+fn my_function_fn<F>(f: F)
+where
+    F: Fn(),
+{
+    f();
 }
+
+fn main() {
+    let my_vec = vec![1, 2, 3, 4, 5];
+    my_function_fn(|| {
+        my_vec
+            .iter()
+            .for_each(|x| println!("Count: {x}"));
+    });
+    my_function_fn(|| {
+        my_vec
+            .iter()
+            .for_each(|x| println!("Doubled: {}", x*2));
+    });
+}
+ 
