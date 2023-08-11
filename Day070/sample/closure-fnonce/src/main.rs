e43986c7ef72d06d06138fcc5ee323070653161b
shinyay <shinya.com@gmail.com>
2023-08-11 12:32:02 +0900

    Add FnOnce sample

--- Day070/sample/closure-fnonce/src/main.rs
+++ Day070/sample/closure-fnonce/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 10;
+    
+    // Using the `move` keyword to capture `x` by value
+    let closure = move || {
+        println!("Closure: x = {}", x);
+    };
+    
+    closure();  // Call the closure
+    
+    // Uncommenting the following line would result in a compilation error
+    // closure();  // Attempting to call the closure again would fail
 }
