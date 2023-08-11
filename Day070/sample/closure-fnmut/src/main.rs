c096fc612d18e8d78d0a3eab3be31c4f40923a30
shinyay <shinya.com@gmail.com>
2023-08-11 12:09:15 +0900

    Add a FnMut sample

--- Day070/sample/closure-fnmut/src/main.rs
+++ Day070/sample/closure-fnmut/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut init_value = 10;
+    let mut my_closure = |x: i32| {
+        println!("Initial Value: {}", init_value);
+        init_value = init_value + x
+    };
+
+    my_closure(10);
+    my_closure(10);
+    my_closure(10);
+    println!("Result: {}", init_value);
+    assert_eq!(40, init_value);
 }
