e387565892ac6873781a5de901ca542b0207ba66
shinyay <shinya.com@gmail.com>
2023-08-11 16:40:55 +0900

    Add a sample code of the closure with FnOnce

--- Day071/sample/closure-fnonce/src/main.rs
+++ Day071/sample/closure-fnonce/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+fn my_function_fnonce<F>(f: F)
+where
+    F: FnOnce(),
+{
+    f();
 }
+
+fn main() {
+    let my_vec = vec![1, 2, 3, 4, 5];
+    my_function_fnonce(|| {
+        my_vec
+            .into_iter()
+            .for_each(|x| println!("Count: {x}"));
+    });
+    // my_function_fnonce(|| {
+    //     my_vec
+    //         .into_iter()
+    //         .for_each(|x| println!("Count: {x}"));
+    // });
+}
+ 
