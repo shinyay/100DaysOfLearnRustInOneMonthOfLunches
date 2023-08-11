ac69c3cc62ce8c489e4c0f40ad615f1b07d778fc
shinyay <shinya.com@gmail.com>
2023-08-11 18:01:35 +0900

    Add three types of closures

--- Day071/sample/closures/src/main.rs
+++ Day071/sample/closures/src/main.rs
@@ -1,3 +1,17 @@
+fn my_function_fnonce<F: FnOnce()>(f: F) {
+    f();
+}
+
+fn my_function_fnmut<F: FnMut()>(mut f: F) {
+    f();
+    f();
+}
+
+fn my_function_fn<F: Fn()>(f: F) {
+    f();
+    f();
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
