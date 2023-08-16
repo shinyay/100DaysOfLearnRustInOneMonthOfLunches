0ee015c8c7ce3938ebc386cefe961e01bcccc12c
shinyay <shinya.com@gmail.com>
2023-08-16 22:01:40 +0900

    Add Trait, Struct and its impl

--- Day075/sample/impl-trait-return/src/main.rs
+++ Day075/sample/impl-trait-return/src/main.rs
@@ -1,3 +1,15 @@
+trait MyTrait {
+    fn do_something(&self);
+}
+
+struct MyStruct;
+
+impl MyTrait for MyStruct {
+    fn do_something(&self) {
+        println!("Doing Something...");
+    }    
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
