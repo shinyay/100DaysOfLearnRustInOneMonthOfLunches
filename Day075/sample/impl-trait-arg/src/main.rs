9a67d53984f0c4b53ece33adcc32d8877aa51cfa
shinyay <shinya.com@gmail.com>
2023-08-16 22:12:02 +0900

    Add a project template for imple Trait as an argument

--- Day075/sample/impl-trait-arg/src/main.rs
+++ Day075/sample/impl-trait-arg/src/main.rs
@@ -1,3 +1,20 @@
+trait MyTrait {
+    fn do_something(&self);
+}
+
+struct MyStruct;
+
+impl MyTrait for MyStruct {
+    fn do_something(&self) {
+        println!("Doing something!!!");
+    }
+}
+
+fn process_my_trait_impl(item: impl MyTrait) {
+    item.do_something();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_struct = MyStruct;
+    process_my_trait_impl(my_struct);
 }
