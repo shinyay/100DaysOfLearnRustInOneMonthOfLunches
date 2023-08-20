338054da5b4c55f624dc577238e46c2c883bc633
shinyay <shinya.com@gmail.com>
2023-08-20 14:58:42 +0900

    Add a simple Arc sample code

--- Day078/sample/simple-arc/src/main.rs
+++ Day078/sample/simple-arc/src/main.rs
@@ -1,3 +1,19 @@
+use std::{sync::Arc, thread};
+
 fn main() {
-    println!("Hello, world!");
+    let data = vec![1, 2, 3, 4, 5];
+    let shared_data = Arc::new(data);
+
+    let thread1_data = shared_data.clone();
+    let thread1 = thread::spawn(move || {
+        println!("[Thread1] {:?}", thread1_data);
+    });
+
+    let thread2_data = shared_data.clone();
+    let thread2 = thread::spawn(move || {
+        println!("[Thread2] {:?}", thread2_data);
+    });
+
+    thread1.join().unwrap();
+    thread2.join().unwrap();
 }
