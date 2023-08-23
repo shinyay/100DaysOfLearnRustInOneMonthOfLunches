7d4489735b5c8f24471977e77d0d01cc889414f6
shinyay <shinya.com@gmail.com>
2023-08-23 19:41:56 +0900

    Add a sample code for loger lifetime

--- Day082/sample/longer-lifetime/src/main.rs
+++ Day082/sample/longer-lifetime/src/main.rs
@@ -1,3 +1,13 @@
+use std::{sync::Arc, thread};
+
 fn main() {
-    println!("Hello, world!");
+    let data = vec![1, 2, 3, 4, 5];
+    let shared_data = Arc::new(data.clone());
+
+    let handle = thread::spawn(move || {
+        println!("ThreadL {:?}", shared_data);
+    });
+
+    handle.join().unwrap();
+    println!("Main Thread: {:?}", data);
 }
