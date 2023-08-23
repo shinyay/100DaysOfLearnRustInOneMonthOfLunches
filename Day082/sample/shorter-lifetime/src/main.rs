636f62f2cdb2fca24b47d90f0a61210686e3577b
shinyay <shinya.com@gmail.com>
2023-08-23 19:49:57 +0900

    Add a sample code for shorter lifetime

--- Day082/sample/shorter-lifetime/src/main.rs
+++ Day082/sample/shorter-lifetime/src/main.rs
@@ -1,3 +1,13 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let data = vec![1, 2, 3, 4, 5];
+    let referenced_data = &data;
+
+    let handle = thread::spawn(move || {
+        println!("Thread: {:?}", referenced_data);
+    });
+
+    handle.join().unwrap();
+    println!("Main Thread: {:?}", data);
 }
