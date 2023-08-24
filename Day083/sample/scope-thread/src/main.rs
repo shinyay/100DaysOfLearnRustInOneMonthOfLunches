16bf81520088e61b443348bd7b2ae05382459d26
shinyay <shinya.com@gmail.com>
2023-08-24 22:38:08 +0900

    Add a sample fpr scope thread

--- Day083/sample/scope-thread/src/main.rs
+++ Day083/sample/scope-thread/src/main.rs
@@ -1,3 +1,32 @@
+use std::{ thread, time };
+
 fn main() {
-    println!("Hello, world!");
+   // create a scope
+   thread::scope(|scope| {
+
+       // spawn first thread
+       scope.spawn(|| {
+           thread::sleep( time::Duration::from_secs(1) );
+           // wait for 1 second before printing "Hello, from thread 1"
+           println!("Hello, from thread 1");
+       });
+
+       // spawn second thread
+       scope.spawn(|| {
+           thread::sleep( time::Duration::from_secs(2) );
+           // wait for 2 seconds before printing "Hello, from thread 2"
+           println!("Hello, from thread 2");
+       });
+
+       // spawn third thread
+       scope.spawn(|| {
+           thread::sleep( time::Duration::from_secs(3) );
+           // wait for 3 seconds before printing "Hello, from thread 3"
+           println!("Hello, from thread 3");
+       });
+   });
+
+   // all threads within the scope has to be closed
+   // for the program to continue
+   println!("All threads completed!");
 }
