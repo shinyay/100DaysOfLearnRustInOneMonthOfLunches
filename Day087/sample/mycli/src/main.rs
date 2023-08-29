4785b0f0fe23d42d7beba90aaa6fcae5b6575b55
shinyay <shinya.com@gmail.com>
2023-08-29 21:35:05 +0900

    Add command line project

--- Day087/sample/mycli/src/main.rs
+++ Day087/sample/mycli/src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use rand::seq::SliceRandom;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 && (args[1] == "-h" || args[1] == "--help") {
+        println!("Usage: comforting-words [name]");
+        println!("Prints a comforting message along with an optional name.");
+        println!("If a name is provided, the message will be addressed to the name.");
+        return;
+    }
+
+    let comforting_words = [
+        "You're doing great!",
+        "Take a deep breath and relax.",
+        "This too shall pass.",
+        "You're stronger than you think.",
+        "Sending you positive vibes.",
+        "You're not alone in this.",
+        "Believe in yourself.",
+        "You've got this!",
+        "Stay positive and keep going.",
+        "You're capable of amazing things.",
+    ];
+
+    let name = args.get(1).cloned();
+
+    let mut rng = rand::thread_rng();
+    let random_message = comforting_words.choose(&mut rng).unwrap();
+
+    match name {
+        Some(name) => println!("{}: {}", name, random_message),
+        None => println!("{}", random_message),
+    }
 }
