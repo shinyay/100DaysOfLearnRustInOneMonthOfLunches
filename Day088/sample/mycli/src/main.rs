85dd7661162248ab488eacc51bf1599ad31b64c6
shinyay <shinya.com@gmail.com>
2023-09-01 18:02:24 +0900

    Add clap sample

--- Day088/sample/mycli/src/main.rs
+++ Day088/sample/mycli/src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(
+    author = "Shinya Yanagihara",
+    version = "0.0.1",
+    about = "My first cli with clap"
+)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
